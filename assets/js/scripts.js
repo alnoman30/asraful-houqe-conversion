315da8eb96d03dfd40ce60f803755323a311679f
Abdullah Al Noman <alnomaan30@gmail.com>
2026-04-08 16:08:59 +0600

    images folder changes

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
+
+document.querySelectorAll(".counter").forEach((counter) => {
+  let target = +counter.getAttribute("data-target");
+  let obj = { val: 0 };
+
+  gsap.to(obj, {
+    val: target,
+    duration: 5,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: counter,
+      start: "top 80%",
+      end: "top 20%",
+      toggleActions: "restart none restart none",
+    },
+    onUpdate: function () {
+      counter.innerText = Math.floor(obj.val) + "+";
+    },
+  });
+});
