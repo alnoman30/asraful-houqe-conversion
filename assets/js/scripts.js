1d2373dc83ba45557b934a42309a8a933b41e72d
Abdullah Al Noman <alnomaan30@gmail.com>
2026-04-08 19:02:05 +0600

    core expertise section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// Counter js
 document.querySelectorAll(".counter").forEach((counter) => {
   let target = +counter.getAttribute("data-target");
   let obj = { val: 0 };
@@ -19,3 +20,46 @@ document.querySelectorAll(".counter").forEach((counter) => {
     },
   });
 });
+
+// Core expertise section: Heading fill animation
+gsap.set(".text-fill", {
+  backgroundSize: "0%"
+});
+const textElements = gsap.utils.toArray(".text-fill");
+
+textElements.forEach((text) => {
+  gsap.fromTo(
+    text,
+    { backgroundSize: "0%" },
+    {
+      backgroundSize: "100%",
+      ease: "none",
+      scrollTrigger: {
+        trigger: text,
+        start: "top 90%",
+        end: "center 50%",
+        scrub: true,
+      }
+    }
+  );
+});
+
+gsap.utils.toArray(".expertise-card").forEach((card, i) => {
+    gsap.fromTo(
+      card,
+      { opacity: 0, y: 50, scale: 0.95 },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        duration: 1,
+        delay: i * 0.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: card,
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      }
+    );
+  });
